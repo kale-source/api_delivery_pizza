15e9f190827dcc6d4b221ff2a3992f1ebeceb900
Kaiky <kaikychimbum11@gmail.com>
2025-07-19 21:07:12 -0300

    create account rout | cryptography passwords | dependencies sessions | starting schemas

--- auth_routes.py
+++ auth_routes.py
@@ -1,11 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from models import Usuario, db # Importando tabela usuário
+from dependences import pegar_sessao
+from main import bcrypt_context
 
 auth_router = APIRouter(prefix='/auth', tags=['auth'])
 
 @auth_router.get('/')
-async def autenticar():
+async def home():
     """
     Essa é a rota padrão de autenticação do nosso sistema. 
     """
     return {'mensagem': 'Você acessou a rota padrão de autenticação', 'autenticado': False}
 
+@auth_router.post("/criar_usuario")
+async def create_account(email: str, password: str, name: str, session=Depends(pegar_sessao)):
+    
+    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+    try:
+        if usuario:
+            raise HTTPException(status_code=400, detail='E-mail do usuário já cadastrado.')
+        else:
+            crypt_password = bcrypt_context.hash(password)
+
+            novo_usuario = Usuario(name, email, crypt_password) # passar na ordem do db
+            session.add(novo_usuario)
+            session.commit()
+            return {'mensagem': f'Email {email} cadastrado com sucesso.'}
+    except:
+        raise HTTPException(status_code=400, detail='E-mail do usuário já cadastrado.')
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
 
-# para rodar o código executar no terminal uvicorn main:app --reload
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 app = FastAPI() # Inicializando o FastAPI
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 # Importar roteadores das rotas
 from auth_routes import auth_router
