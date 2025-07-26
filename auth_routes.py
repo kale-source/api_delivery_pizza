3374dfc7d69d8653120ac81a7ba73b3102b279a5
Kaiky <kaikychimbum11@gmail.com>
2025-07-26 20:57:13 -0300

    first schema 'UsuarioSchema'

--- auth_routes.py
+++ auth_routes.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario, db # Importando tabela usuário
 from dependences import pegar_sessao
 from main import bcrypt_context
+from schemas import UsuarioSchema
+from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix='/auth', tags=['auth'])
 
@@ -13,18 +15,18 @@ async def home():
     return {'mensagem': 'Você acessou a rota padrão de autenticação', 'autenticado': False}
 
 @auth_router.post("/criar_usuario")
-async def create_account(email: str, password: str, name: str, session=Depends(pegar_sessao)):
+async def create_account(usuario_schema: UsuarioSchema, session: Session = Depends(pegar_sessao)):
     
-    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+    usuario = session.query(Usuario).filter(Usuario.email == usuario_schema.email).first()
     try:
         if usuario:
             raise HTTPException(status_code=400, detail='E-mail do usuário já cadastrado.')
         else:
-            crypt_password = bcrypt_context.hash(password)
+            crypt_password = bcrypt_context.hash(usuario_schema.password)
 
-            novo_usuario = Usuario(name, email, crypt_password) # passar na ordem do db
+            novo_usuario = Usuario(usuario_schema.name, usuario_schema.email, crypt_password, usuario_schema.status, usuario_schema.admin) # passar na ordem do db
             session.add(novo_usuario)
             session.commit()
-            return {'mensagem': f'Email {email} cadastrado com sucesso.'}
+            return {'mensagem': f'Email {usuario_schema.email} cadastrado com sucesso.'}
     except:
         raise HTTPException(status_code=400, detail='E-mail do usuário já cadastrado.')
--- schemas.py
+++ schemas.py
@@ -1,1 +1,13 @@
-from pydantic import *
+from pydantic import BaseModel
+from typing import Optional
+
+class UsuarioSchema(BaseModel):
+    name: str
+    email: str
+    password: str
+    status: Optional[bool]
+    admin: Optional[bool]
+
+    class Config:
+        from_attributes = True
+        
