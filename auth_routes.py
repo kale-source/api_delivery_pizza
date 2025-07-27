eae26d0fee8d9f5c89bbc85c5142da3805efe5a5
Kaiky <kaikychimbum11@gmail.com>
2025-07-27 00:10:38 -0300

    Create JWT Authentication | access_token and refresh_token

--- auth_routes.py
+++ auth_routes.py
@@ -1,22 +1,39 @@
 from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario # Importando tabela usuário
 from dependences import pegar_sessao
-from main import bcrypt_context
+from main import bcrypt_context, ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM, SECRET_KEY
 from schemas import UsuarioSchema, LoginSchema
 from sqlalchemy.orm import Session
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
 
 auth_router = APIRouter(prefix='/auth', tags=['auth'])
 
-def create_token(id_usuario):
-    token = 'asdi7gafd80h923j8941j2asiodhgioasduhg'+f'{id_usuario}'
-    return token
+def create_token(id_usuario, token_duration=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
+    # Token JWT
+    
+    expiration_date = datetime.now(timezone.utc) + token_duration
+    dict_info = {"sub": id_usuario, 'exp': expiration_date} # Por padrão tem que deixar EXP para reconhecer que é um datetime
+
+    encoded_jwt = jwt.encode(dict_info, key=SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
+
+def authenticate_token(token, session: Session = Depends(pegar_sessao)):
+    #check = jwt.decode(token, SECRET_KEY, ALGORITHM)
+    
+    usuario = session.query(Usuario).filter(Usuario.id == 1).first()
+
+    return usuario
 
 @auth_router.get('/')
 async def home():
     """
     Essa é a rota padrão de autenticação do nosso sistema. 
     """
-    return {'mensagem': 'Você acessou a rota padrão de autenticação', 'autenticado': False}
+    return {
+        'mensagem': 'Você acessou a rota padrão de autenticação', 'autenticado': False
+        }
 
 @auth_router.post("/criar_usuario")
 async def create_account(usuario_schema: UsuarioSchema, session: Session = Depends(pegar_sessao)):
@@ -31,7 +48,9 @@ async def create_account(usuario_schema: UsuarioSchema, session: Session = Depen
             novo_usuario = Usuario(usuario_schema.name, usuario_schema.email, crypt_password, usuario_schema.status, usuario_schema.admin) # passar na ordem do db
             session.add(novo_usuario)
             session.commit()
-            return {'mensagem': f'Email {usuario_schema.email} cadastrado com sucesso.'}
+            return {
+                'mensagem': f'Email {usuario_schema.email} cadastrado com sucesso.'
+                }
     except:
         raise HTTPException(status_code=400, detail='E-mail do usuário já cadastrado.')
 
@@ -44,11 +63,25 @@ async def login(login_schema: LoginSchema, session: Session = Depends(pegar_sess
         
         if check_password:
             access_token = create_token(usuario.id)
+            refresh_token = create_token(usuario.id, timedelta(days=7))
 
-            return {'access_token': access_token, 'token_type': 'Bearer'}
-        
-            # retornar token necessário
+            return {
+                    'access_token': access_token, 
+                    'refresh': refresh_token, 
+                    'token_type': 'Bearer'
+                    }
         else:
             raise HTTPException(status_code=400, detail='Email ou senha inválidos.')
     else:
         raise HTTPException(status_code=400, detail='Email ou senha inválidos.')
+
+@auth_router.get("/refresh")
+async def use_refresh_token(token):
+    usuario = authenticate_token(token) # Falta configurar a depends para passar como parametro para funcionar o refresh token
+
+    access_token = create_token(usuario.id)
+
+    return {
+            'access_token': access_token, 
+            'token_type': 'Bearer'
+            }
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
 
 app = FastAPI() # Inicializando o FastAPI
 
