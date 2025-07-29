fa0b1450b868675a7e57e3bc36bdac84f9f91471
Kaiky <kaikychimbum11@gmail.com>
2025-07-28 22:47:47 -0300

    orders authentication | add cancel order rout

--- order_routes.py
+++ order_routes.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from dependences import pegar_sessao
+from dependences import pegar_sessao, authenticate_token
 from schemas import PedidoSchema
-from models import Pedidos
+from models import Pedidos, Usuario
 
-order_router = APIRouter(prefix='/pedidos', tags=['pedidos'])
+order_router = APIRouter(prefix='/pedidos', tags=['pedidos'], dependencies=[Depends(authenticate_token)])
 
 @order_router.get('/')
 async def pedidos():
@@ -14,13 +14,39 @@ async def pedidos():
     return {'mensagem': 'Você acessou a rota de pedidos!'}
     
 @order_router.post('/pedido')
-async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(pegar_sessao)):
-    criar_pedido = Pedidos(user_id=pedido_schema.user_id)
+async def criar_pedido(session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(authenticate_token)):
+    # Falta definir pedidos, entre outros.
+    create_order = Pedidos(user_id = usuario.id)
 
-    session.add(criar_pedido)
+    session.add(create_order)
     session.commit()
 
-    return {'mensagem': 'Pedido criado com sucesso!', 'id_pedido': f'ID do pedido: {criar_pedido.id}'}
+    return {
+        'mensagem': 'Pedido criado com sucesso!', 
+        'id_pedido': f'ID do pedido: {create_order.id}',
+        'id_usuario': usuario.id
+        }
+
+@order_router.post('/pedido/cancelar/{id_pedido}')
+async def cancelar_pedido(id_pedido: int, session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(authenticate_token)):
+    order = session.query(Pedidos).filter(Pedidos.id == id_pedido).first()
+    user_id = session.query(Usuario).filter(Usuario.id == usuario.id).first()
+
+    if not user_id.admin and user_id.id != order.user_id: 
+        raise HTTPException(status_code=401, detail='Usuário não possui permissão necessária.')
+    elif not order:
+        raise HTTPException(status_code=400, detail='Pedido não encontrado.')
+    elif order.status == 'CANCELADO':
+        raise HTTPException(status_code=401, detail='Pedido já foi cancelado em outro momento.')
+    
+    order.status = 'CANCELADO'
+    session.commit()
+
+    return {
+        'mensagem': f'Pedido {order.id} foi cancelado com sucesso.',
+        'order': order
+        }
+
 
 
 
